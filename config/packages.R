ee1c72449e7650795d67f3363886b5f1ca5c13f2
mramnath-fe <Manasi.Ramnath@frontier-economics.com>
2025-03-04 16:49:21 +0000

    edited package dependencies and conflicts

--- config/packages.R
+++ config/packages.R
@@ -1,3 +1,4 @@
+if (!requireNamespace("conflicted", quietly = TRUE)) install.packages("conflicted")
 library(conflicted)
 # conflicted packcages
 conflicts_prefer(dplyr::filter,
@@ -9,10 +10,19 @@ if (!requireNamespace("pacman", quietly = TRUE)) install.packages("pacman")
 pacman::p_load(
   readODS, installr, tidyverse, zip, janitor, readxl, writexl, openxlsx,
   car, tidymodels, plm, readr, broom.mixed, lmtest, multiwayvcov, tidypredict, 
-  fe, ggplot2, Metrics, caret, gridExtra, foreach, plotly,
+  ggplot2, Metrics, caret, gridExtra, foreach, plotly,
   sp, sf, tmap, RColorBrewer, stargazer
 )
 
+# install fe package
+if (!requireNamespace("fe", quietly = TRUE)) {
+  if (.Platform$OS.type == "unix") {
+    install.packages("~/Z/Internal Projects/X-practice resources/fe_github_packages/fe_r", repos=NULL, type="source")
+  } else {
+    install.packages("Z:/Projects/Internal Projects/X-practice resources/fe_github_packages/fe_r", repos=NULL, type="source")
+  }
+}
 
+library(fe)
 
 
